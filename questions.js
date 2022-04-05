137732688c8c8ec3456ea26ffbac1c062ae89d19
Matthew Carson <matthew.carson@pm.me>
2022-04-05 08:53:51 -0400

    Add in licenses

--- questions.js
+++ questions.js
@@ -1,8 +1,10 @@
+import licenses from './licenses.js'
+
 export default [
   {
     name: 'title',
     message: 'Project Title:',
-    validate: (input, answers) =>  {
+    validate: input =>  {
       if (!input.length) return 'A Project Title is required'
       return true
     },
@@ -12,21 +14,52 @@ export default [
     name: 'description',
     message: 'Description:',
     type: 'editor',
+    validate: input =>  {
+      if (!input.length) return 'A description is required'
+      return true
+    },
     filter: input => `## Description\n${input}`
   },
-  // {
-  //   name: 'instructions',
-  //   message: 'Installation instructions' 
-  // },
-  // {
-  //   name: 'usage',
-  //   message: 'Usuage information'
-  // },
-  // {
-  //   name: 'guidelines',
-  //   message: 'Contribution guidelines'
-  // }, {
-  //   name: 'test',
-  //   message: 'Test instructions'
-  // }
+  {
+    name: 'installation',
+    message: 'Installation instructions' ,
+    filter: input => `## Installation Instructions\n${input}`
+  },
+  {
+    name: 'usage',
+    message: 'Usuage information',
+    filter: input => `## Usage\n${input}`
+  },
+  {
+    name: 'contribution',
+    message: 'Contribution guidelines',
+    filter: input => `## Contribution Guidelines\n${input}`
+  }, {
+    name: 'test',
+    message: 'Test instructions',
+    filter: input => `## Tests\n${input}`
+  },
+  {
+    name: 'license',
+    message: 'Choose a license',
+    type: 'list',
+    choices: ['MIT', 'ISC', 'GNU GPLv3', 'Apache License 2.0'],
+    filter: input => {
+      return `## License\n${licenses[input]}`.replace(/\s4/g, '')
+    },
+  },
+  {
+    name: 'Github',
+    message: 'Github username',
+    filter: (input, answers) => {
+      answers.questions = `#Questions?\n Have additional questions? I can be reached at [Github](https://github.com/${input})`
+    }
+  },
+  {
+    name: 'email',
+    message: 'E-mail address',
+    filter: (input, answers) => {
+      answers.questions += ` or you can e-mail me at ${input}.`
+    }
+  }
 ]
