8fea33a6d6353ee65479677ba199cf98069d1543
Matthew Carson <matthew.carson@pm.me>
2022-04-07 12:03:41 -0400

    More cleanup

--- src/Builder.js
+++ src/Builder.js
@@ -4,15 +4,15 @@ import { license } from './questions.js'
 export default class ReadMeBuilder {
   constructor(answers) {
     this.answers = answers
-    this.doNotInclude = ['github', 'email', 'sections', 'title']
+    this.ignore = ['github', 'email', 'sections', 'title']
     this.body = ''
   }
 
-  tableOfContents() {
+  createTableOfContents() {
     let tableOfContents = '## Table of Contents\n'
 
     Object.keys(this.answers).forEach(answer => {
-      if (this.doNotInclude.includes(answer)) return
+      if (this.ignore.includes(answer)) return
       tableOfContents += `- [${answer}](#${answer})\n`
     })
 
@@ -22,9 +22,9 @@ export default class ReadMeBuilder {
   createBody() {
     let section = `# ${this.answers['title']}\n
 ![License](https://img.shields.io/badge/License-${license}-white?labelColor=green&style=flat)\n
-${this.tableOfContents(this.answers)}\n`
+${this.createTableOfContents(this.answers)}\n`
     for (const answer in this.answers) {
-      if (!this.doNotInclude.includes(answer)) {
+      if (!this.ignore.includes(answer)) {
         section += `## ${answer}\n`
         // Add the license body into the generated README
         if (answer === 'License') {
